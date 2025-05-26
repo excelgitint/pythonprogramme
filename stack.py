790c5cf0936238d80a7ca7e46f19ab3f8f911693
excelgitint <lokeshtandur7@gmail.com>
2025-05-26 11:40:44 +0530

    Add files via upload

---
+++ stack.py
@@ -0,0 +1,19 @@
+Stack = []
+print(Stack)
+Stack.append(10)
+print(Stack)
+Stack.append(20)
+print(Stack)
+Stack.append(30)
+print("removing ELEments")
+Stack.pop()
+print(Stack)
+Stack.pop()
+print(Stack)
+Stack.pop()
+print(Stack)
+
+print("done")
+
+
+
